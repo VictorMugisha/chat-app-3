11bf730dec7bfbe46e57a2ea316bcb51dc5cf77c
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-13 12:11:17 +0200

    Register user route handler is working

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
 
-const app = express();
-const PORT = process.env.PORT || 5000;
-dotenv.config();
-
 import authRoutes from "./routes/auth.routes.js";
 import connectDB from "./db/connectDB.js";
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
-    connectDB();
-    console.log(`Server started on port ${PORT}`);
+  connectDB();
+  console.log(`Server started on port ${PORT}`);
 });
