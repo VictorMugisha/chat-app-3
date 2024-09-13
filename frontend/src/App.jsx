b923e3eed383e6c33df4fdb8128fecda82fc5bb1
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-13 18:53:54 +0200

    Designed most of the home component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Home from "./pages/home/Home";
 // import Login from "./pages/login/Login";
-import SignUp from "./pages/signup/Signup";
+// import SignUp from "./pages/signup/Signup";
 
 function App() {
   return <div className="flex h-screen items-center justify-center p-4">
-    <SignUp />
+    {/* <SignUp /> */}
+    <Home />
   </div>;
 }
 
---
+++ frontend/src/components/sidebar/Conversations.jsx
@@ -0,0 +1,13 @@
+import Conversation from "./Conversation";
+
+export default function Conversations() {
+  return (
+    <div className="flex flex-col overflow-auto py-2">
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+        <Conversation />
+    </div>
+  )
+}
