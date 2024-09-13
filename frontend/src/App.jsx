8a51b773e1cef8d4efe15100bc20f432bcc4f07e
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-13 20:33:39 +0200

    Save a user from the frontend using proxy server

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,21 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/home/Home";
+import SignUp from "./pages/signup/SignUp";
+import Login from "./pages/login/Login";
+import { Toaster } from "react-hot-toast";
 
 function App() {
-  return <div className="flex h-screen items-center justify-center p-4">
-    <Home />
-  </div>;
+  return (
+    <div className="flex h-screen items-center justify-center p-4">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/login" element={<Login />}/>
+        <Route path="/signup" element={<SignUp />}/>
+      </Routes>
+      <Toaster />
+    </div>
+  );
 }
 
 export default App;
--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -1,16 +1,34 @@
-export default function GenderCheckbox() {
+export default function GenderCheckbox({ onCheckboxChange, selectedGender }) {
   return (
     <div className="flex">
       <div className="form-control">
-        <label className="label gap-2 cursor-pointer">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "male" ? "selected" : ""
+          }`}
+        >
           <span className="label-text text-white">Male</span>
-          <input type="checkbox" className="checkbox border-slate-200" />
+          <input
+            type="checkbox"
+            className="checkbox border-slate-200"
+            checked={selectedGender === "male"}
+            onChange={() => onCheckboxChange("male")}
+          />
         </label>
       </div>
       <div className="form-control">
-        <label className="label gap-2 cursor-pointer">
+        <label
+          className={`label gap-2 cursor-pointer ${
+            selectedGender === "female" ? "selected" : ""
+          }`}
+        >
           <span className="label-text text-white">Female</span>
-          <input type="checkbox" className="checkbox border-slate-200" />
+          <input
+            type="checkbox"
+            className="checkbox border-slate-200"
+            checked={selectedGender === "female"}
+            onChange={() => onCheckboxChange("female")}
+          />
         </label>
       </div>
     </div>
