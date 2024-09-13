16e6f237ebf7e6068b8a9d90747917aa79cd3c90
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-13 11:53:36 +0200

    Connected to database

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,11 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 import authRoutes from "./routes/auth.routes.js";
+import connectDB from "./db/connectDB.js";
 
 app.use("/api/auth", authRoutes);
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server started on port ${PORT}`);
+});
