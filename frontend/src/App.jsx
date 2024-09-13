3bd02cee15f32760165c057cff8f3258249a7e86
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-13 18:59:03 +0200

    Designed most of the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,8 @@
 import "./App.css";
 import Home from "./pages/home/Home";
-// import Login from "./pages/login/Login";
-// import SignUp from "./pages/signup/Signup";
 
 function App() {
   return <div className="flex h-screen items-center justify-center p-4">
-    {/* <SignUp /> */}
     <Home />
   </div>;
 }
--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -23,7 +23,7 @@ export default function MessageContainer() {
   );
 }
 
-const NoChatSelected = () => {
+function NoChatSelected(){
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
