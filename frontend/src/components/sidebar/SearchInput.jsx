50c0b0885bebbce8defb58e892aeccb3c164da84
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-16 17:09:33 +0200

    Fixed SearchInput component

--- frontend/src/components/sidebar/SearchInput.jsx
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -8,31 +8,31 @@ export default function SearchInput() {
   const { setSelectedConversation } = useConversation();
   const { conversations } = useGetConversations();
 
+  async function handleSubmit(e) {
+  
   if (!search) return;
   if (search.length < 3) {
     return toast.error("Search term must be at least 3 characters");
   }
-  async function handleSubmit(e) {
     e.preventDefault();
+    const conversation = conversations.find((c) =>
+      c.fullName.toLowerCase().includes(search.toLowerCase())
+    );
+  
+    if (conversation) {
+      setSelectedConversation(conversation);
+      setSearch("");
+    } else {
+      toast.error("No conversation found");
+    }
   }
 
-  const conversation = conversations.find((c) =>
-    c.fullName.toLowerCase().includes(search.toLowerCase())
-  );
-
-  if (conversation) {
-    setSelectedConversation(conversation);
-    setSearch("");
-  } else {
-    toast.error("No conversation found");
-  }
   return (
     <form className="flex items-center gap-2" onSubmit={handleSubmit}>
-      <h1>Search is here</h1>
       <input
         type="text"
         placeholder="Search..."
-        className="input input-bordered rounded-full text-white"
+        className="input input-bordered rounded-full"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
       />
